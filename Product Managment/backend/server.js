7a67003c2834e104d6ea6c805cc6c4b63d5f786a
Mr Lii <leetamparega@gmail.com>
2025-01-20 15:31:22 +0000

    changes made

--- Product Managment/backend/server.js
+++ Product Managment/backend/server.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.send("hello")
+app.post('/api/products', async (req,res) =>{
+    const product = req.body;
+
+    if( !product.name || !product.price || !product.image){
+        return res.status(400).json({succes:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({succes: true, data: newProduct});
+    } catch (error){
+        console.log("Error in Creating product:", error.message);
+        res.status(500).json({succes: false, message:'Server error'});
+    }
 })
 
 app.listen(5000, ()=>{
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cool_ones
+
+a repo to hold both my old and new web projects
+
